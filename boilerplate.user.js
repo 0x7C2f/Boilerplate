4069af2cdbe8c58a76c2e8c309ce2cf64bd7438f
0x7C2f <mrgrim@grimdevelopment.com>
2023-04-07 10:09:02 -0400

    update boilerplate.user.js

--- boilerplate.user.js
+++ boilerplate.user.js
@@ -1,15 +1,69 @@
 // ==UserScript==
 // @name         0x7C2f Boilerplate
-// @namespace    http://tampermonkey.net/
 // @version      1.0
+// @updateURL    https://raw.githubusercontent.com/0x7C2f/Boilerplate/main/boilerplate.user.js
 // @description  My personal userscripts boilerplate.
 // @author       0x7C2f
-// @match        *://*.*/*
+// @homepageURL     https://github.com/0x7C2f/Boilerplate
+// @supportURL      https://github.com/0x7C2f/Boilerplate/discussions
+// @include      *
+// @namespace    https://github.com/0x7C2f/Boilerplate
+
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+// Function to load jquery
+function addJQuery(callback) {
+	var script = document.createElement("script");
+
+	script.setAttribute(
+		"src",
+		"//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"
+	);
+
+	script.addEventListener(
+		"load",
+		function () {
+			var script = document.createElement("script");
+			script.textContent =
+				"window.jQ=jQuery.noConflict(true);(" +
+				callback.toString() +
+				")();";
+			document.body.appendChild(script);
+		},
+		false
+	);
+	document.body.appendChild(script);
+}
+// Prevent Visibility Tracking
+function visibility() {
+	window.addEventListener(
+		"visibilitychange",
+		function (event) {
+			event.stopImmediatePropagation();
+		},
+		true
+	);
+	window.addEventListener(
+		"blur",
+		function (event) {
+			event.stopImmediatePropagation();
+		},
+		true
+	);
+	window.addEventListener(
+		"mouseleave",
+		function (event) {
+			event.stopImmediatePropagation();
+		},
+		true
+	);
+}
 
 
-})();
+// the guts of this userscript
+function main() {
+}
+
+addJQuery(main);
+visibility();
