c4aa2b41ae5db3005268284f30b757a11ea17164
0x7C2f <mrgrim@grimdevelopment.com>
2023-04-07 11:00:15 -0400

    update README.md and boilerplate.user.js

--- boilerplate.user.js
+++ boilerplate.user.js
@@ -8,62 +8,73 @@
 // @supportURL      https://github.com/0x7C2f/Boilerplate/discussions
 // @include      *
 // @namespace    https://github.com/0x7C2f/Boilerplate
-
 // @grant        none
 // ==/UserScript==
 
+// Array of blacklisted URLs
+var blacklistedUrls = [
+  "https://example.com/blacklisted-url1",
+  "https://example.com/blacklisted-url2",
+  // Add more blacklisted URLs here
+];
+
 // Function to load jquery
 function addJQuery(callback) {
-	var script = document.createElement("script");
+  var script = document.createElement("script");
 
-	script.setAttribute(
-		"src",
-		"//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"
-	);
+  script.setAttribute(
+    "src",
+    "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"
+  );
 
-	script.addEventListener(
-		"load",
-		function () {
-			var script = document.createElement("script");
-			script.textContent =
-				"window.jQ=jQuery.noConflict(true);(" +
-				callback.toString() +
-				")();";
-			document.body.appendChild(script);
-		},
-		false
-	);
-	document.body.appendChild(script);
+  script.addEventListener(
+    "load",
+    function () {
+      var script = document.createElement("script");
+      script.textContent =
+        "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+      document.body.appendChild(script);
+    },
+    false
+  );
+  document.body.appendChild(script);
 }
 // Prevent Visibility Tracking
 function visibility() {
-	window.addEventListener(
-		"visibilitychange",
-		function (event) {
-			event.stopImmediatePropagation();
-		},
-		true
-	);
-	window.addEventListener(
-		"blur",
-		function (event) {
-			event.stopImmediatePropagation();
-		},
-		true
-	);
-	window.addEventListener(
-		"mouseleave",
-		function (event) {
-			event.stopImmediatePropagation();
-		},
-		true
-	);
+  window.addEventListener(
+    "visibilitychange",
+    function (event) {
+      event.stopImmediatePropagation();
+    },
+    true
+  );
+  window.addEventListener(
+    "blur",
+    function (event) {
+      event.stopImmediatePropagation();
+    },
+    true
+  );
+  window.addEventListener(
+    "mouseleave",
+    function (event) {
+      event.stopImmediatePropagation();
+    },
+    true
+  );
 }
 
+// Check if the current URL is blacklisted
+function isBlacklistedUrl(url) {
+  return blacklistedUrls.includes(url);
+}
 
 // the guts of this userscript
 function main() {
+  var currentUrl = window.location.href;
+  if (!isBlacklistedUrl(currentUrl)) {
+    visibility();
+  }
 }
 
 addJQuery(main);
-visibility();
