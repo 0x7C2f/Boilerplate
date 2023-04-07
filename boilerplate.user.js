e88a77004f09b29f765ab1a52b1c0577eefee34b
0x7C2f <mrgrim@grimdevelopment.com>
2023-04-07 09:35:23 -0400

    docs: update README.md

---
+++ boilerplate.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         0x7C2f Boilerplate
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  My personal userscripts boilerplate.
+// @author       0x7C2f
+// @match        *://*.*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
